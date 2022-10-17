06966730040d5956fef749b2565e25aed3faac63
Daalbu <daalbu@post.cz>
2022-10-17 20:50:54 +0200

    Initial commit
    Add class
    SmartChess
    
    Add main

---
+++ src/cz/devschool/SmartChess/SmartChess.java
@@ -0,0 +1,14 @@
+package cz.devschool.SmartChess;
+
+import java.util.Scanner;
+
+public class SmartChess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Zadejte velikost policka:");
+        int fieldSize = scanner.nextInt();
+        System.out.println("Zadejte velikost sachovnice:");
+        int boardSize = scanner.nextInt();
+
+    }
+}
