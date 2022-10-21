797cf40b0bfdadf3a901aed58b640bb7fda28002
Daalbu <daalbu@post.cz>
2022-10-21 22:04:01 +0200

    Add method printBoard

--- src/cz/devschool/SmartChess/SmartChess.java
+++ src/cz/devschool/SmartChess/SmartChess.java
@@ -9,6 +9,63 @@ public class SmartChess {
         int fieldSize = scanner.nextInt();
         System.out.println("Zadejte velikost sachovnice:");
         int boardSize = scanner.nextInt();
+        printBoard(fieldSize, boardSize);
 
+    }
+    public static void printBoard(int fieldSize, int boardSize) {
+        //int x = 0;
+        System.out.println(fieldSize);
+        System.out.println(boardSize);
+
+        System.out.print('+');
+        for (int i = 0; i < boardSize; i++)
+            System.out.print('-');
+        System.out.println('+');
+
+        if(boardSize % 2 == 1){
+            System.out.print('|');
+            for (int i = 0; i < boardSize / 2; i++) {
+                for (int x = 0; x < boardSize / 2; x++) {
+                    System.out.print('x');
+                    System.out.print(' ');
+                }
+                System.out.print('x');
+                System.out.print("|\n|");
+
+                for (int x = 0; x < boardSize / 2; x++) {
+                    System.out.print(' ');
+                    System.out.print('x');
+                }
+                System.out.print(' ');
+                System.out.print("|\n|");
+            }
+            for (int x = 0; x < boardSize / 2; x++) {
+                System.out.print('x');
+                System.out.print(' ');
+            }
+            System.out.print('x');
+            System.out.print("|\n");
+
+        }else{
+
+            for (int i = 0; i < boardSize / 2; i++) {
+                System.out.print('|');
+                for (int x = 0; x < boardSize / 2; x++) {
+                    System.out.print('x');
+                    System.out.print(' ');
+                }
+                System.out.print("|\n|");
+
+                for (int x = 0; x < boardSize / 2; x++) {
+                    System.out.print(' ');
+                    System.out.print('x');
+                }
+                System.out.print("|\n");
+            }
+        }
+        System.out.print('+');
+        for (int i = 0; i < boardSize; i++)
+            System.out.print('-');
+        System.out.println('+');
     }
 }
