bc965c3044a70a080b44b221d47c60e49292c329
Daalbu <daalbu@post.cz>
2022-10-22 11:44:09 +0200

    Add comments

--- src/cz/devschool/SmartChess/SmartChess.java
+++ src/cz/devschool/SmartChess/SmartChess.java
@@ -4,24 +4,23 @@ import java.util.Scanner;
 
 public class SmartChess {
     public static void main(String[] args) {
+        //scans for user input and saves to variables
         Scanner scanner = new Scanner(System.in);
         System.out.println("Zadejte velikost policka:");
         int fieldSize = scanner.nextInt();
         System.out.println("Zadejte velikost sachovnice:");
         int boardSize = scanner.nextInt();
+
         printBoard(fieldSize, boardSize);
-
     }
-    public static void printBoard(int fieldSize, int boardSize) {
-        //int x = 0;
-        System.out.println(fieldSize);
-        System.out.println(boardSize);
-
+    public static void printBoard(int fieldSize, int boardSize) {  //method prints the Chess Board by SmartChess™ :)
+        //printing the top half of the frame
         System.out.print('+');
         for (int i = 0; i < boardSize * fieldSize; i++)
             System.out.print('-');
         System.out.println('+');
 
+        //printing the fields and the frame
         for (int i = 0; i < boardSize / 2; i++) {
             for (int j = 0; j < fieldSize; j++) {
                 System.out.print('|');
@@ -34,6 +33,7 @@ public class SmartChess {
                         System.out.print(' ');
                     }
                 }
+                //in case of odd board size, the last field is printed
                 if (boardSize % 2 ==1) {
                     for (int k = 0; k < fieldSize; k++) {
                         System.out.print('x');
@@ -42,6 +42,7 @@ public class SmartChess {
                 System.out.println('|');
             }
 
+            //printing the even lines
             for (int j = 0; j < fieldSize; j++) {
                 System.out.print('|');
                 for (int k = 0; k < boardSize / 2; k++) {
@@ -61,6 +62,8 @@ public class SmartChess {
                 System.out.println('|');
             }
         }
+
+        //printing the last line in case of odd number
         if (boardSize % 2 ==1) {
             for (int j = 0; j < fieldSize; j++) {
                 System.out.print('|');
@@ -80,6 +83,7 @@ public class SmartChess {
             }
         }
 
+        //priting the lower half of the frame
         System.out.print('+');
         for (int i = 0; i < boardSize * fieldSize; i++)
             System.out.print('-');
